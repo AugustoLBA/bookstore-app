fc90031b5a68db3b5a1e59bd5c4431e7b29b3932
Augusto LBA <j.augusto.lba@gmail.com>
2024-06-10 12:16:25 -0300

    adicionando o metodo para salvar os livros na classe BookService

--- src/main/java/com/bookstore/jpa/service/BookService.java
+++ src/main/java/com/bookstore/jpa/service/BookService.java
@@ -1,7 +1,43 @@
 package com.bookstore.jpa.service;
 
+import com.bookstore.jpa.dtos.BookRecordDTO;
+import com.bookstore.jpa.models.BookModel;
+import com.bookstore.jpa.models.ReviewModel;
+import com.bookstore.jpa.repositories.AuthorRepository;
+import com.bookstore.jpa.repositories.BookRepository;
+import com.bookstore.jpa.repositories.PublisherRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BookService(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
+    }
+    @Transactional
+    public BookModel saveBook(BookRecordDTO recordDTO){
+        BookModel book = new BookModel();
+
+        book.setTitle(recordDTO.title());
+        book.setPublisher(publisherRepository.findById(recordDTO.publisherId()).get());
+        book.setAuthors(authorRepository.findAllById(recordDTO.authorsIds()).stream().collect(Collectors.toSet()));
+
+        ReviewModel review = new ReviewModel();
+
+        review.setBook(book);
+        review.setComment(recordDTO.reviewComment());
+        book.setReview(review);
+        return book;
+    }
 }
